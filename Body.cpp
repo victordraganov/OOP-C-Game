69938898eb40200c8c550d28244dbab6187a793a
Victor Draganov <veke.draganov@gmail.com>
2015-05-21 05:20:33 +0300

    Create Body.cpp

---
+++ Body.cpp
@@ -0,0 +1,31 @@
+#include"Body.h"
+
+Body::Body(std::vector<ColorBlock> blocks, int width, int height) {
+	for (int i = 0; i < blocks.size(); i++)
+	{
+		AddBlock(blocks[i]);
+	}
+
+	this->width = width;
+	this->height = height;
+}
+
+Body::Body(const Body& other) {
+	this->width = other.Width();
+	this->height = other.Height();
+	this->blocks = other.blocks;
+}
+
+Body::~Body() {}
+
+void Body::AddBlock(ColorBlock block) {
+	this->blocks.push_back(block);
+}
+
+int Body::Width() const{
+	return this->width;
+}
+
+int  Body::Height() const {
+	return this->height;
+}
