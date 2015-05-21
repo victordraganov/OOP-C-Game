d9b908ca728fb05a62f42911d723800db93f8134
Victor Draganov <veke.draganov@gmail.com>
2015-05-21 05:23:14 +0300

    Create CommandHandler.h

---
+++ CommandHandler.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class CommandHandler{
+public:
+	char GetPrimaryCommand(); // Gets the menu commands and player1 commands
+
+	char GetPlayer2Command(); // Gets player2 commands if there is currently second player
+
+	//void HandlePlayerOne(); // Handles 
+
+	//void HandlePlayerTwo(); //
+
+};
